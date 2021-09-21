3fe20ac7cd3f2a87692ec2c28565f7481fb1590f
hechengyao <hechengyao@bonc.com.cn>
2021-09-21 20:35:41 +0800

    Commit:   拦截器

---
+++ src/main/java/com/lanhuigu/springboot2study/config/AdminWebConfig.java
@@ -0,0 +1,32 @@
+package com.lanhuigu.springboot2study.config;
+
+import com.lanhuigu.springboot2study.interceptor.LoginInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jiuzhoumu
+ * @date 2021/9/21 19:20
+ */
+
+/**
+ * 1、编写一个拦截器，实现HandlerInterceptor接口
+ * 2、拦截器注册到容器中（实现WebMvcConfigurer的addInterceptors）
+ * 3、指定拦截器的拦截规则
+ */
+@Configuration
+public class AdminWebConfig implements WebMvcConfigurer {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminWebConfig.class);
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**") // 所有的请求都被拦截，包括静态资源
+                .excludePathPatterns("/", "login", "/css/**", "/fonts/**", "/images/**", "/js/**"); // 放行的资源
+    }
+}
---
+++ src/main/java/com/lanhuigu/springboot2study/interceptor/LoginInterceptor.java
@@ -0,0 +1,75 @@
+package com.lanhuigu.springboot2study.interceptor;
+
+import com.lanhuigu.springboot2study.po.User;
+import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Jiuzhoumu
+ * @date 2021/9/21 19:06
+ */
+
+/**
+ * 登陆检查
+ * 1、配置好拦截器要拦截哪些请求
+ * 2、把这些配置放在容器中
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    
+    private static final Logger log = LoggerFactory.getLogger(LoginInterceptor.class);
+    
+    /**
+     * 目标方法执行之前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("执行LoginInterceptor的preHandle方法，handler = {}", handler);
+        // 登录检查逻辑
+        HttpSession session = request.getSession();
+        User loginUser = (User) session.getAttribute("loginUser");
+        if (null != loginUser) {
+            // 放行
+            return true;
+        }
+        // 拦截。未登录。跳转到登录页
+        // session.setAttribute("msg","请先登录");
+        // response.sendRedirect("/login");
+        request.setAttribute("msg", "请先登录");
+        request.getRequestDispatcher("/").forward(request, response);
+        return false;
+    }
+    
+    /**
+     * 目标方法执行之后
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("执行LoginInterceptor的postHandle方法，modelAndView = {}", modelAndView);
+    }
+    
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        log.info("执行LoginInterceptor的postHandle方法，异常ex = {}", ex);
+    }
+}
