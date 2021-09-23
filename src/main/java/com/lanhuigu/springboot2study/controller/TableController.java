83e8f0ebc9e9c0ededfbdbcda89bd4bf8cec2078
hechengyao <hechengyao@aliyun.com>
2021-09-23 17:45:18 +0800

    Modify:   前端抽取公共部分时，前后端交互的controller

--- src/main/java/com/lanhuigu/springboot2study/controller/TableController.java
+++ src/main/java/com/lanhuigu/springboot2study/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.lanhuigu.springboot2study.controller;
 
+import com.lanhuigu.springboot2study.po.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Jiuzhoumu
@@ -9,12 +14,38 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class TableController {
-    
+
     @GetMapping("/basic_table")
-    public String basic_table(){
-        
+    public String basic_table() {
         return "table/basic_table";
     }
-    
-    
+
+    @GetMapping("/dynamic_table")
+    public String dynamic_table(Model model) {
+
+        List<User> users = Arrays.asList(new User("yidaqi", "yidaqi123456"),
+                new User("java", "java123456"),
+                new User("zookeeper", "zookeeper123456"),
+                new User("hadoop", "hadoop123456"),
+                new User("kafka", "kafka123456"));
+        model.addAttribute("users", users);
+        return "table/dynamic_table";
+    }
+
+    @GetMapping("/editable_table")
+    public String editable_table() {
+        return "table/editable_table";
+    }
+
+    @GetMapping("/pricing_table")
+    public String pricing_table() {
+        return "table/pricing_table";
+    }
+
+    @GetMapping("/responsive_table")
+    public String responsive_table() {
+        return "table/responsive_table";
+    }
+
+
 }
