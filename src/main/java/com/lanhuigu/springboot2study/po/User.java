b639c8c3659a8d4d11c8f9bcb043b95dcc297ed1
hechengyao <hechengyao@aliyun.com>
2021-09-23 17:42:23 +0800

    Modify:   添加了构造方法的注解

--- src/main/java/com/lanhuigu/springboot2study/po/User.java
+++ src/main/java/com/lanhuigu/springboot2study/po/User.java
@@ -1,11 +1,15 @@
 package com.lanhuigu.springboot2study.po;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author: HeChengyao
  * @date: 2021/7/21 11:20
  */
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class User {
 
