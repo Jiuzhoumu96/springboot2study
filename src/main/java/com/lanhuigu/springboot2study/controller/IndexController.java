3aa99191463dd22cc41507ea2d5ebc37106587d2
hechengyao <hechengyao@aliyun.com>
2021-09-23 19:36:07 +0800

    Modify:   拦截器

--- src/main/java/com/lanhuigu/springboot2study/controller/IndexController.java
+++ src/main/java/com/lanhuigu/springboot2study/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.lanhuigu.springboot2study.controller;
 
 import com.lanhuigu.springboot2study.po.User;
-import lombok.val;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,39 +17,34 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 public class IndexController {
-    
+
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
-    
-    @GetMapping(value = {"/", "/login"})
+
+    // @GetMapping(value = {"/", "/login"})
+    @GetMapping(value = "/testss")
     public String loginPage() {
-        
+        log.info(">>>>>> IndexController.loginPage");
         return "login";
     }
-    
+
     @PostMapping("/login")
     public String main(HttpSession session, User user, Model model) {
-        if (StringUtils.isNotBlank(user.getUserName().trim()) && StringUtils.equals(user.getPassWord().trim(),"123456")) {
+        if (StringUtils.isNotBlank(user.getUserName().trim()) && StringUtils.equals(user.getPassWord().trim(), "123")) {
+            log.info(">>>>>> IndexController.main");
             session.setAttribute("loginUser", user);
+            log.info(">>>>>> user = {}", user);
             //登陆成功，重定向到main.html，重定向防止表单重复提交
             return "redirect:/main.html";
-        }else {
-            model.addAttribute("msg","账号密码错误");
+        } else {
+            model.addAttribute("msg", "账号密码错误");
             return "login";
         }
-        
-        
     }
-    
+
     @GetMapping("/main.html")
-    public String mainPage(HttpSession session,Model model) {
-        // User loginUser = (User) session.getAttribute("loginUser");
-        // if(null != loginUser){
-        //     return "main";
-        // }else {
-        //     model.addAttribute("msg","请重新登录");
-        //     return "login";
-        // }
+    public String mainPage(HttpSession session, Model model) {
+        log.info(">>>>>> IndexController.mainPage");
         return "main";
     }
-    
+
 }
--- src/main/java/com/lanhuigu/springboot2study/interceptor/LoginInterceptor.java
+++ src/main/java/com/lanhuigu/springboot2study/interceptor/LoginInterceptor.java
@@ -1,7 +1,6 @@
 package com.lanhuigu.springboot2study.interceptor;
 
 import com.lanhuigu.springboot2study.po.User;
-import lombok.val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -22,9 +21,9 @@ import javax.servlet.http.HttpSession;
  * 2、把这些配置放在容器中
  */
 public class LoginInterceptor implements HandlerInterceptor {
-    
+
     private static final Logger log = LoggerFactory.getLogger(LoginInterceptor.class);
-    
+
     /**
      * 目标方法执行之前
      *
@@ -36,22 +35,26 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("执行LoginInterceptor的preHandle方法，handler = {}", handler);
+        log.info(">>>>>> 执行LoginInterceptor.LoginInterceptor的preHandle方法，handler = {}", handler);
+
+        String requestUrl = request.getRequestURI();
+        log.info(">>>>>> 拦截的请求的路径是 {}", requestUrl);
+
         // 登录检查逻辑
         HttpSession session = request.getSession();
         User loginUser = (User) session.getAttribute("loginUser");
         if (null != loginUser) {
+            log.info(">>>>>> loginUser = {}", loginUser);
             // 放行
             return true;
         }
         // 拦截。未登录。跳转到登录页
-        // session.setAttribute("msg","请先登录");
-        // response.sendRedirect("/login");
+        log.info(">>>>>> 拦截。未登录。跳转到登录页");
         request.setAttribute("msg", "请先登录");
         request.getRequestDispatcher("/").forward(request, response);
         return false;
     }
-    
+
     /**
      * 目标方法执行之后
      *
@@ -64,12 +67,12 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         // HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-        log.info("执行LoginInterceptor的postHandle方法，modelAndView = {}", modelAndView);
+        log.info(">>>>>> 执行LoginInterceptor.LoginInterceptor的postHandle方法，modelAndView = {}", modelAndView);
     }
-    
+
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         // HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-        log.info("执行LoginInterceptor的postHandle方法，异常ex = {}", ex);
+        log.info(">>>>>> 执行LoginInterceptor.LoginInterceptor的postHandle方法，异常ex = {}", ex);
     }
 }
