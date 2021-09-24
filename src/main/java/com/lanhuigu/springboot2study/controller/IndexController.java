1a3d949ef1a7c69547eead8da55e92afbc2fe556
hechengyao <hechengyao@aliyun.com>
2021-09-24 17:18:06 +0800

    Modify:   swagger2的功能体现

--- src/main/java/com/lanhuigu/springboot2study/controller/IndexController.java
+++ src/main/java/com/lanhuigu/springboot2study/controller/IndexController.java
@@ -1,6 +1,10 @@
 package com.lanhuigu.springboot2study.controller;
 
 import com.lanhuigu.springboot2study.po.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,17 +19,29 @@ import javax.servlet.http.HttpSession;
  * @author Jiuzhoumu
  * @date 2021/9/20 23:42
  */
+@Api(value = "登录", tags = {"用户操作接口"})
+//tags表示说明；value也是说明，可以使用tags替代
 @Controller
 public class IndexController {
 
     private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 
-    @GetMapping(value = {"/", "/login"})
+    @ApiOperation(value = "login", notes = "登录")
+    // value用于方法描述；notes用于提示内容；tags可以重新分组（视情况而用）
+    @GetMapping(value = "/login")
+    // @GetMapping(value = {"/", "/login"})
     public String loginPage() {
         log.info(">>>>>> IndexController.loginPage");
         return "login";
     }
 
+    @ApiOperation(value = "登录", notes = "输入用户名，密码")
+    // value用于方法描述；notes用于提示内容；tags可以重新分组（视情况而用）
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "session", value = "会话", required = true, dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "user", value = "用户", required = true, dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "model", value = "model", required = true, dataType = "String", paramType = "query")})
+    // name参数名；value参数说明；required是否必填；dataType数据类型；paramType参数类型；example举例说明
     @PostMapping("/login")
     public String main(HttpSession session, User user, Model model) {
         if (StringUtils.isNotBlank(user.getUserName().trim()) && StringUtils.equals(user.getPassWord().trim(), "123")) {
@@ -40,6 +56,12 @@ public class IndexController {
         }
     }
 
+    @ApiOperation(value = "main", notes = "进入main页面")
+    // value用于方法描述；notes用于提示内容；tags可以重新分组（视情况而用）
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "session", value = "会话", required = true, dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "model", value = "model", required = true, dataType = "String", paramType = "query")})
+    // name参数名；value参数说明；required是否必填；dataType数据类型；paramType参数类型；example举例说明
     @GetMapping("/main.html")
     public String mainPage(HttpSession session, Model model) {
         log.info(">>>>>> IndexController.mainPage");
